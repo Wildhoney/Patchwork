7920269161963f0ce5bfcfb5fe689eb43018821c
Wildhoney <adam.timberlake@gmail.com>
2015-01-09 14:02:40 +0000

    Added CSS sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,17 @@
     const COMPONENT_FILES = ['components/Patchwork.scss'];
 
     // Gulp Dependencies...
-    var gulp   = require('gulp'),
-        sass   = require('gulp-sass');
+    var gulp       = require('gulp'),
+        sass       = require('gulp-sass'),
+        rename     = require('gulp-rename'),
+        sourcemaps = require('gulp-sourcemaps');
 
     gulp.task('build-sass', function () {
 
         gulp.src(COMPONENT_FILES)
+            .pipe(sourcemaps.init())
             .pipe(sass())
+            .pipe(sourcemaps.write())
             .pipe(rename('patchwork.css'))
             .pipe(gulp.dest('dist'))
             .pipe(gulp.dest('example/vendor/patchwork'));
